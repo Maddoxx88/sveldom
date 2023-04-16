e4f456e50f54e4b4691fc2a4c31688d980f7b890
Sunit Shirke <sunitshirke88@gmail.com>
2023-04-16 22:52:26 +0530

    style: added tailwind support

---
+++ client/src/lib/components/landing.svelte
@@ -0,0 +1,10 @@
+<div class="flex">
+    <div class="flex flex-col">
+        <h1 class="text-3xl">Online Mobile Games</h1>
+    </div>
+    <div></div>
+</div>
+
+<script>
+    // your script goes here
+</script>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Header from '$lib/header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+
+<slot />
